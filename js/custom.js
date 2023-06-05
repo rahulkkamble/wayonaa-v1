2fe576015daa827c514ce45b9021f3dddbda4b52
faizalimulla <faizalimulla@gmail.com>
2023-06-05 14:43:25 +0530

    index fixed

--- js/custom.js
+++ js/custom.js
@@ -36,18 +36,5 @@ document.addEventListener('DOMContentLoaded', function () {
   ScrollReveal().reveal('.intro-cards .intro-card-3, .award-logo-3', { delay: 500, duration: 2000, opacity: 0, distance: "50%", origin: "bottom" });    /* easing: 'ease-in-out', */
   ScrollReveal().reveal('#astino-banner .large-heading, #astino-banner p, #feature .heading-wrapper, #variation .heading-wrapper, #usp .large-heading, #dealer .heading-wrapper', { delay: 200, duration: 1000, opacity: 0, distance: "40%", origin: "bottom" });    /* easing: 'ease-in-out', */
   // ScrollReveal().reveal('#about .text-block',{delay: 300, duration: 1000, opacity: 0, distance: "50%", origin: "bottom", reset: true});
-
-  var hrefGOne = document.getElementById('model-g-one-path');
-  var hrefGLite = document.getElementById('model-g-lite-path');
-  var hrefDealer = document.getElementById('dealer-path');
-  var hrefAbout = document.getElementById('about-path');
-  var hrefAstino = document.getElementById('astino-path');
-  var hrefBecomeADealer = document.getElementById('become-a-dealer-path');
-  hrefBecomeADealer.setAttribute('href','assets/route/become-a-dealer.html');
-  hrefDealer.setAttribute('href','assets/route/dealer.html');
-  hrefGOne.setAttribute('href','assets/route/g-one.html');
-  hrefGLite.setAttribute('href','assets/route/g-lite.html');
-  hrefAbout.setAttribute('href','assets/route/about.html');
-  hrefAstino.setAttribute('href','assets/route/astino.html');
 }, true);
 
