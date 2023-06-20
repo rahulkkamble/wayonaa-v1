3a426620510135da1280c98be1431033d371a086
faizalimulla <faizalimulla@gmail.com>
2023-06-20 12:31:11 +0530

    hovered class addition in custom js file, become a dealer page footer image changed

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,26 @@
+var feedbackCards = document.querySelectorAll(".customer-feedback-cards .card");
+console.log(feedbackCards);
+
+if (feedbackCards) {
+  feedbackCards.forEach(function (card) {
+    card.addEventListener("mouseover", function () {
+      card.classList.add("hovered");
+      setTimeout(function(){
+        feedbackCards[0].classList.add("hovered");
+    },3000);
+    });
+
+    card.addEventListener("mouseout", function () {
+      card.classList.remove("hovered");
+    });
+  });
+  
+  // feedbackCards.forEach(function (card) {
+    
+  // });
+  
+}
+
 // ** REVEAL ANIMATIONS FOR 
 document.addEventListener('DOMContentLoaded', function () {
   ScrollReveal().reveal('#hero-bg-1', { delay: 1000, duration: 1000, opacity: 0, distance: "10%", origin: "left" });    /* easing: 'ease-in-out', */
@@ -7,16 +30,16 @@ document.addEventListener('DOMContentLoaded', function () {
   ScrollReveal().reveal('.img-wrapper, .text-block, .btn-wrapper, .heading-wrapper, .icon-with-heading, #g-one-model-g, #g-lite-model-g', {
     delay: 300, duration: 1000, opacity: 0, distance: "60%", origin: "bottom",
   });
-  
+
   ScrollReveal().reveal('.intro-cards .intro-card-1, .award-logo-1', { delay: 100, duration: 1000, opacity: 0, distance: "30%", origin: "bottom" });    /* easing: 'ease-in-out', */
   ScrollReveal().reveal('.intro-cards .intro-card-2, .award-logo-2', { delay: 300, duration: 1500, opacity: 0, distance: "40%", origin: "bottom" });    /* easing: 'ease-in-out', */
   ScrollReveal().reveal('.intro-cards .intro-card-3, .award-logo-3', { delay: 500, duration: 2000, opacity: 0, distance: "50%", origin: "bottom" });    /* easing: 'ease-in-out', */
   // ScrollReveal().reveal('#about .text-block',{delay: 300, duration: 1000, opacity: 0, distance: "50%", origin: "bottom", reset: true});
-  ScrollReveal().reveal('#astino-banner .large-heading, #astino-banner p, #feature .heading-wrapper, #variation .heading-wrapper, #usp .large-heading, #dealer .heading-wrapper, .large-heading, #model-g .animation-p', 
-  { delay: 200, duration: 1000, opacity: 0, distance: "40%", origin: "bottom" });    /* easing: 'ease-in-out', */
+  ScrollReveal().reveal('#astino-banner .large-heading, #astino-banner p, #feature .heading-wrapper, #variation .heading-wrapper, #usp .large-heading, #dealer .heading-wrapper, .large-heading, #model-g .animation-p',
+    { delay: 200, duration: 1000, opacity: 0, distance: "40%", origin: "bottom" });    /* easing: 'ease-in-out', */
 
   ScrollReveal().reveal('.persons-info, .breadcrumb, .card-map-section .card, .mapouter, .btn', {
-    delay: 200, duration: 1000, opacity: 0, distance: "40%", origin: "bottom", useDelay: 'always' 
+    delay: 200, duration: 1000, opacity: 0, distance: "40%", origin: "bottom", useDelay: 'always'
   });
 
   // scale to 0
